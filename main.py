ce37bb0a20251216ad4c9672edbc4fdc84736cd0
poikilos <7557867+poikilos@users.noreply.github.com>
2024-07-18 20:19:31 -0400

    Switch to non-interactive mode if a file is specified.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+import sly
 import sys
 
 from basic import BasicInterpreter
 
 
-def main_interactive():
+def print_title():
     print("Cman's simple BASIC v0.1")
     print('2019 Cheaterman')
 
@@ -13,6 +15,44 @@ def main_interactive():
 
     except ImportError:
         pass
+
+
+def run_file(path):
+    print_title()
+
+    interpreter = BasicInterpreter()
+    parent = os.path.dirname(path)
+    os.chdir(parent)
+    lines = []
+    with open(path, 'r') as stream:
+        for raw in stream:
+            lines.append(raw.rstrip("\r\n"))
+    line_index = -1
+    for line in lines:
+        line_index += 1
+        if not line.strip():
+            # Avoid "SyntaxError: Unexpected EOF"
+            continue
+        try:
+            interpreter.interpret(line)
+        except SyntaxError as exception:
+            print('"{}" line {}: {}: {}'
+                  .format(path, line_index+1,
+                          type(exception).__name__, exception))
+        except sly.lex.LexError as exception:
+            print('"{}" line {}: {}: {}'
+                  .format(path, line_index+1,
+                          type(exception).__name__, exception))
+        except KeyboardInterrupt:
+            if interpreter.running_program:
+                print(f'Break in {interpreter.last_program_lineno}')
+
+    print('Bye!')
+    return 0
+
+
+def main_interactive():
+    print_title()
 
     interpreter = BasicInterpreter()
 
@@ -46,5 +86,14 @@ def main_interactive():
     return 0
 
 
+def main():
+    path = None
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    if path:
+        return run_file(path)
+    return main_interactive()
+
+
 if __name__ == '__main__':
-    sys.exit(main_interactive())
+    sys.exit(main())
